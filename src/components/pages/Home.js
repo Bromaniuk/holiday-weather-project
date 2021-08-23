e0135720e583c836cbf371d5bbf1d73e69300210
Brian Romaniuk <brirom@telus.net>
2021-08-23 14:39:52 -0700

    Create base Home page component

---
+++ src/components/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../App.css';
+import Search from '../Search';
+
+function Home() {
+    return (
+        <div className='home-wrapper'>
+            <Search />
+        </div>
+    )
+};
+
+export default Home;
