884557697dc66865c5e3c6c676a208d4518af3a0
Brian Romaniuk <brirom@telus.net>
2021-08-28 16:52:09 -0700

    Add app routing for weather page

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,18 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/pages/Home';
+import Weather from './components/pages/Weather'
+import Search from './components/Search';
 
 function App() {
   return (
     <>
     <Router>
       <Navbar />
+      <Search />
       <Switch>
         <Route path='/' exact component={Home} />
+        <Route path='/weather' component={Weather} />
       </Switch>
     </Router>
     </>
