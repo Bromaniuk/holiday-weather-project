c83080959720ef5fe1296a520fbaa824a4486df9
Brian Romaniuk <brirom@telus.net>
2021-08-31 13:59:07 -0700

    Add containers to style App properly

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import './App.css';
-import Navbar from './components/Navbar';
+import { Link } from 'react-router-dom';
 import Home from './components/pages/Home';
 import Weather from './components/pages/Weather'
 import Search from './components/Search';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
 
 function App() {
   return (
     <>
-    <Router>
-      <Navbar />
-      <Search />
-      <Switch>
-        <Route path='/' exact component={Home} />
-        <Route path='/weather/:country/:city' component={Weather} />
-      </Switch>
-    </Router>
+      <Router>
+        <div className='content-wrapper'>
+          <div className='header'><Link to='/' className='header-link'>Holiday Weather</Link></div>
+          <div className='search-wrapper'>
+            <Search />
+          </div>
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/weather/:country/:city' component={Weather} />
+          </Switch>
+        </div>
+      </Router>
     </>
   );
 }
