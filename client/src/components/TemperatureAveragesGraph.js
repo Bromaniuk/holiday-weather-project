15459c847ab7ca00d420e7fa21c5544155e0ab45
Brian Romaniuk <brirom@telus.net>
2021-09-01 14:34:42 -0700

    Change BarGraph to LineGraph, add high/low values

--- client/src/components/TemperatureAveragesGraph.js
+++ client/src/components/TemperatureAveragesGraph.js
@@ -1,24 +1,37 @@
 import React from 'react';
-import { ResponsiveContainer, BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar } from 'recharts';
+import { ResponsiveContainer, LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from 'recharts';
 
 
-function MonthlyAveragesGraph({ toggleTemp, data, clickFunction }) {
+function MonthlyAveragesGraph({ toggleTemp, celsiusData, fahrenheitData }) {
 
     return (
-        <div className='month-avg-graph'>
-            <ResponsiveContainer width="100%" height="100%">
-                <BarChart data={data}>
-                    <CartesianGrid strokeDasharray="4" />
-                    <XAxis dataKey="name" />
-                    <YAxis type="number" domain={['dataMin', 'auto']}/>
-                    <Tooltip />
-                    <Legend />
-                    {toggleTemp
-                        ? <Bar dataKey="celsius" fill="#8884d8" onClick={clickFunction} cursor={'pointer'}/>
-                        : <Bar dataKey="fahrenheit" fill="#82ca9d" onClick={clickFunction} cursor={'pointer'}/>
-                    }
-                </BarChart>
-            </ResponsiveContainer>
+        <div className='month-avg-graph' >
+            {toggleTemp
+                ? <ResponsiveContainer width="100%" height="100%" >
+                    <LineChart data={celsiusData}>
+                        <CartesianGrid strokeDasharray="4" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Line dataKey="median" fill="#8884d8" stroke='#8884d8' />
+                        <Line dataKey="low" fill="#82ca9d" stroke='#82ca9d' />
+                        <Line dataKey="high" fill="#ff0000" stroke='#ff0000' />
+                    </LineChart>
+                </ResponsiveContainer>
+                : <ResponsiveContainer width="100%" height="100%">
+                    <LineChart data={fahrenheitData}>
+                        <CartesianGrid strokeDasharray="4" />
+                        <XAxis dataKey="name" />
+                        <YAxis type="number" domain={['auto', 'auto']} />
+                        <Tooltip />
+                        <Legend />
+                        <Line dataKey="median" fill="#8884d8" stroke='#8884d8' />
+                        <Line dataKey="low" fill="#82ca9d" stroke='#82ca9d' />
+                        <Line dataKey="high" fill="#ff0000" stroke='#ff0000' />
+                    </LineChart>
+                </ResponsiveContainer>
+            }
         </div>
     )
 };
