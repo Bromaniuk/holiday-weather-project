20273ae9a267069ce346fe6d6f78113ba50fb01b
Brian Romaniuk <brirom@telus.net>
2021-09-01 14:27:20 -0700

    Edit getWeather function to return converted temperature data

--- dao/weatherDAO.js
+++ dao/weatherDAO.js
@@ -20,39 +20,77 @@ export default class WeatherDAO {
         city
     } = {}) {
         let query = { "city": { $eq: city } }
-        
+
         let cursor
 
         try {
             cursor = await weather.collection(country)
-            .findOne(query)
-        } catch(e) {
+                .findOne(query)
+        } catch (e) {
             console.error(`Unable to issue find command, ${e}`)
-            return { monthlyAverages: [] }
+            return {
+                celsiusMonthlyAveragesMedianTableData: [],
+                celsiusMonthlyAveragesMinTableData: [],
+                celsiusMonthlyAveragesMaxTableData: [],
+
+                fahrenheitMonthlyAveragesMedianTableData: [],
+                fahrenheitMonthlyAveragesMinTableData: [],
+                fahrenheitMonthlyAveragesMaxTableData: [],
+
+                celsiusMonthlyAveragesGraphData: [],
+                fahrenheitMonthlyAveragesGraphData: [],
+
+                celsiusDailyAveragesGraphData: [],
+                fahrenheitDailyAveragesGraphData: []
+            }
         }
 
         const displayCursor = cursor
 
         try {
-            const monthlyAverages = []
-            const monthlyAveragesGraphData = []
-            const dailyAveragesGraphData = []
+            const celsiusMonthlyAveragesMedianTableData = []
+            const celsiusMonthlyAveragesMinTableData = []
+            const celsiusMonthlyAveragesMaxTableData = []
+
+            const fahrenheitMonthlyAveragesMedianTableData = []
+            const fahrenheitMonthlyAveragesMinTableData = []
+            const fahrenheitMonthlyAveragesMaxTableData = []
+
+            const celsiusMonthlyAveragesGraphData = []
+            const fahrenheitMonthlyAveragesGraphData = []
+
+            const celsiusDailyAveragesGraphData = []
+            const fahrenheitDailyAveragesGraphData = []
+
+
             const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-            for (let month = 1 ; month < 13 ; month++) {
-                // monthlyAverages is an array of median Kelvin temperatures for each month for selected city
-                // Celsius and Fahrenheit calculations are done in Weather.js page
-                monthlyAverages.push(await displayCursor["temp"]["month"][`${month}`]["month_average"])
+            for (let month = 1; month < 13; month++) {
+                celsiusMonthlyAveragesMedianTableData.push(+((await displayCursor["temp"]["month"][`${month}`]["month_average_median"] - 273.15).toFixed(2)))
+                celsiusMonthlyAveragesMinTableData.push(+((await displayCursor["temp"]["month"][`${month}`]["month_average_min"] - 273.15).toFixed(2)))
+                celsiusMonthlyAveragesMaxTableData.push(+((await displayCursor["temp"]["month"][`${month}`]["month_average_max"] - 273.15).toFixed(2)))
 
-                // monthlyAveragesGraphData is an array of json objects that contain the month's name and cel/fah temperatures for the month average
-                // Structured to fit the Rechart's BarGraph data object requirements
-                monthlyAveragesGraphData.push({"name": months[month-1], "celsius": +((await displayCursor["temp"]["month"][`${month}`]["month_average"] - 273.15).toFixed(2)), "fahrenheit": +(((await displayCursor["temp"]["month"][`${month}`]["month_average"] - 273.15) * (9 / 5) + 32).toFixed(2))})
-                
-                // dailyAveragesGraphData creates data objects for Rechart's Bargraph with the individual day's median temps in cel/fah
-                // the below for loop is the length of the month document minus one because within the month document there is a JSON object for
-                // each individual day plus one value for the month average, which is obtained above.
-                let currentMonth = []
-                for (let day = 1 ; day < Object.keys(await displayCursor["temp"]["month"][`${month}`]).length - 1 ; day++) {
+                fahrenheitMonthlyAveragesMedianTableData.push(+(((await displayCursor["temp"]["month"][`${month}`]["month_average_median"] - 273.15) * (9 / 5) + 32).toFixed(2)))
+                fahrenheitMonthlyAveragesMinTableData.push(+(((await displayCursor["temp"]["month"][`${month}`]["month_average_min"] - 273.15) * (9 / 5) + 32).toFixed(2)))
+                fahrenheitMonthlyAveragesMaxTableData.push(+(((await displayCursor["temp"]["month"][`${month}`]["month_average_max"] - 273.15) * (9 / 5) + 32).toFixed(2)))
 
+
+                celsiusMonthlyAveragesGraphData.push({
+                    "name": months[month - 1],
+                    "median": +((await displayCursor["temp"]["month"][`${month}`]["month_average_median"] - 273.15).toFixed(2)),
+                    "low": +((await displayCursor["temp"]["month"][`${month}`]["month_average_min"] - 273.15).toFixed(2)),
+                    "high": +((await displayCursor["temp"]["month"][`${month}`]["month_average_max"] - 273.15).toFixed(2))
+                })
+                fahrenheitMonthlyAveragesGraphData.push({
+                    "name": months[month - 1],
+                    "median": +(((await displayCursor["temp"]["month"][`${month}`]["month_average_median"] - 273.15) * (9 / 5) + 32).toFixed(2)),
+                    "low": +(((await displayCursor["temp"]["month"][`${month}`]["month_average_min"] - 273.15) * (9 / 5) + 32).toFixed(2)),
+                    "high": +(((await displayCursor["temp"]["month"][`${month}`]["month_average_max"] - 273.15) * (9 / 5) + 32).toFixed(2))
+                })
+
+                let celsiusCurrentMonthDailyAveragesGraphData = []
+                let fahrenheitCurrentMonthDailyAveragesGraphData = []
+
+                for (let day = 1; day < Object.keys(await displayCursor["temp"]["month"][`${month}`]).length - 3; day++) {
                     // Below if statements are to correctly format the numeric string of `month-day` by adding a 0 before single digits
                     let currentDay = ''
                     if (month < 10) {
@@ -65,16 +103,50 @@ export default class WeatherDAO {
                     } else {
                         currentDay += `-${day}`
                     }
-                    currentMonth.push({"name": currentDay, "celsius" : +((await displayCursor["temp"]["month"][`${month}`][`${day}`]["median"] - 273.15).toFixed(2)), "fahrenheit": +(((await displayCursor["temp"]["month"][`${month}`][`${day}`]["median"] - 273.15) * (9 / 5) + 32).toFixed(2))})
+
+                    celsiusCurrentMonthDailyAveragesGraphData.push({
+                        "name": currentDay,
+                        "median": +((await displayCursor["temp"]["month"][`${month}`][`${day}`]["median"] - 273.15).toFixed(2)),
+                        "low": +((await displayCursor["temp"]["month"][`${month}`][`${day}`]["average_min"] - 273.15).toFixed(2)),
+                        "high": +((await displayCursor["temp"]["month"][`${month}`][`${day}`]["average_max"] - 273.15).toFixed(2))
+                    })
+
+                    fahrenheitCurrentMonthDailyAveragesGraphData.push({
+                        "name": currentDay,
+                        "median": +(((await displayCursor["temp"]["month"][`${month}`][`${day}`]["median"] - 273.15) * (9 / 5) + 32).toFixed(2)),
+                        "low": +(((await displayCursor["temp"]["month"][`${month}`][`${day}`]["average_min"] - 273.15) * (9 / 5) + 32).toFixed(2)),
+                        "high": +(((await displayCursor["temp"]["month"][`${month}`][`${day}`]["average_max"] - 273.15) * (9 / 5) + 32).toFixed(2))
+                    })
+
                 }
-                dailyAveragesGraphData.push(currentMonth)
+                celsiusDailyAveragesGraphData.push(celsiusCurrentMonthDailyAveragesGraphData)
+                fahrenheitDailyAveragesGraphData.push(fahrenheitCurrentMonthDailyAveragesGraphData)
             }
-            return { monthlyAverages, monthlyAveragesGraphData, dailyAveragesGraphData }
+            return {
+                celsiusMonthlyAveragesMedianTableData, celsiusMonthlyAveragesMinTableData, celsiusMonthlyAveragesMaxTableData,
+                fahrenheitMonthlyAveragesMedianTableData, fahrenheitMonthlyAveragesMinTableData, fahrenheitMonthlyAveragesMaxTableData,
+                celsiusMonthlyAveragesGraphData, fahrenheitMonthlyAveragesGraphData,
+                celsiusDailyAveragesGraphData, fahrenheitDailyAveragesGraphData
+            }
         } catch (e) {
             console.error(
-                `Issue in WeatherDAO`
+                e
             )
-            return { monthlyAverages: [], monthlyAveragesGraphData: [], dailyAveragesGraphData: [] }
+            return {
+                celsiusMonthlyAveragesMedianTableData: [],
+                celsiusMonthlyAveragesMinTableData: [],
+                celsiusMonthlyAveragesMaxTableData: [],
+
+                fahrenheitMonthlyAveragesMedianTableData: [],
+                fahrenheitMonthlyAveragesMinTableData: [],
+                fahrenheitMonthlyAveragesMaxTableData: [],
+
+                celsiusMonthlyAveragesGraphData: [],
+                fahrenheitMonthlyAveragesGraphData: [],
+
+                celsiusDailyAveragesGraphData: [],
+                fahrenheitDailyAveragesGraphData: []
+            }
         }
 
     }
