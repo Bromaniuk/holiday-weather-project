ec7aea78304ac828e400080657508617fdf07eef
Brian Romaniuk <brirom@telus.net>
2021-09-01 16:55:23 -0700

    Trying to deploy to Heroku...

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ MongoClient.connect(
     const port = process.env.PORT || 8000;
     app.use(express.static(path.join(__dirname, 'build')))
 
-    app.get('/*', (req, res) => {
+    app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
     })
 
