d473fb669e08530993be78e4fe3cee5058890251
Brian Romaniuk <brirom@telus.net>
2021-09-02 14:56:49 -0700

    Fix legend rendering issue

--- client/src/components/TemperatureAveragesGraph.js
+++ client/src/components/TemperatureAveragesGraph.js
@@ -1,36 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ResponsiveContainer, LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from 'recharts';
 
 
 function MonthlyAveragesGraph({ toggleTemp, celsiusData, fahrenheitData }) {
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        if (celsiusData.length > 0 && fahrenheitData.length > 0) {
+            setLoading(true);
+        }
+    }, [celsiusData, fahrenheitData])
 
     return (
         <div className='month-avg-graph' >
-            {toggleTemp
-                ? <ResponsiveContainer width="100%" height="100%" >
-                    <LineChart data={celsiusData}>
-                        <CartesianGrid strokeDasharray="4" />
-                        <XAxis dataKey="name" />
-                        <YAxis />
-                        <Tooltip />
-                        <Legend />
-                        <Line dataKey="median" fill="#8884d8" stroke='#8884d8' />
-                        <Line dataKey="low" fill="#82ca9d" stroke='#82ca9d' />
-                        <Line dataKey="high" fill="#ff0000" stroke='#ff0000' />
-                    </LineChart>
-                </ResponsiveContainer>
-                : <ResponsiveContainer width="100%" height="100%">
-                    <LineChart data={fahrenheitData}>
-                        <CartesianGrid strokeDasharray="4" />
-                        <XAxis dataKey="name" />
-                        <YAxis type="number" domain={['auto', 'auto']} />
-                        <Tooltip />
-                        <Legend />
-                        <Line dataKey="median" fill="#8884d8" stroke='#8884d8' />
-                        <Line dataKey="low" fill="#82ca9d" stroke='#82ca9d' />
-                        <Line dataKey="high" fill="#ff0000" stroke='#ff0000' />
-                    </LineChart>
-                </ResponsiveContainer>
+            {loading
+                ? [toggleTemp
+                    ? <ResponsiveContainer width="100%" height="100%" key={0} >
+                        <LineChart data={celsiusData}>
+                            <CartesianGrid strokeDasharray="4" />
+                            <XAxis dataKey="name" />
+                            <YAxis />
+                            <Tooltip />
+                            <Legend />
+                            <Line dataKey="median" fill="#82ca9d" stroke='#82ca9d' />
+                            <Line dataKey="low" fill="#8884d8" stroke='#8884d8' />
+                            <Line dataKey="high" fill="#ff0000" stroke='#ff0000' />
+                        </LineChart>
+                    </ResponsiveContainer>
+                    : <ResponsiveContainer width="100%" height="100%" key={1} >
+                        <LineChart data={fahrenheitData}>
+                            <CartesianGrid strokeDasharray="4" />
+                            <XAxis dataKey="name" />
+                            <YAxis />
+                            <Tooltip />
+                            <Legend />
+                            <Line dataKey="median" fill="#82ca9d" stroke='#82ca9d' />
+                            <Line dataKey="low" fill="#8884d8" stroke='#8884d8' />
+                            <Line dataKey="high" fill="#ff0000" stroke='#ff0000' />
+                        </LineChart>
+                    </ResponsiveContainer>
+                ]
+                : <span></span>
             }
         </div>
     )
