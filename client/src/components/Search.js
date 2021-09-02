0e1469a138153e97f9d6b5a27b4e3837e82ba79d
Brian Romaniuk <brirom@telus.net>
2021-09-02 14:56:18 -0700

    Clear input box after selection

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,23 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Autocomplete, { createFilterOptions } from '@material-ui/lab/Autocomplete';
 import { TextField } from '@material-ui/core';
 import cityList from '../city-list';
+import { useHistory } from 'react-router-dom';
 
 
 const filterOptions = createFilterOptions({
     matchFrom: 'any',
     limit: 10,
-  });
+});
 
 function Search() {
+    const history = useHistory();
+
+    const [clearInput, setClearInput] = useState(false);
 
     return (
         <Autocomplete
+            key={clearInput}
             id="combo-box"
             options={cityList}
             getOptionLabel={(option) => option.label}
             onChange={(event, option) => {
-                window.location.href = `/weather/${option.country}/${option.city}`
+                if (option !== null) {
+                    history.push(`/weather/${option.country}/${option.city}`)
+                    setClearInput(!clearInput);
+                }
             }}
             style={{ width: 300 }}
             renderInput={(params) => <TextField {...params} label="Enter your city" variant="outlined" />}
