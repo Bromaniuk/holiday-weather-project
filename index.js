5e78cfe10ad03e95661b1d8c741b5cce71cbad17
Brian Romaniuk <brirom@telus.net>
2021-09-01 18:00:29 -0700

    Last try b4 i cri :)

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 dotenv.config();
 const MongoClient = mongodb.MongoClient;
-const port = 5000 || 8000;
+const port = process.env.PORT || 8000;
 MongoClient.connect(
   'mongodb+srv://admin:7RYZIGcAEgJvnClB@cluster0.3uxtl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   {
@@ -22,12 +22,11 @@ MongoClient.connect(
     process.exit(1);
   })
   .then(async client => {
-    if (process.env.NODE_ENV === 'production') {
       app.use(express.static(path.join(__dirname, 'build')))
       app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+        res.sendFile(path.join(__dirname, 'build', 'index.html'))
       })
-    }
+    
     await WeatherDAO.injectDB(client);
     app.listen(port, () => {
       console.log(`listening on port ${port}`);
