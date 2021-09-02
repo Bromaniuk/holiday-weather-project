44193803bf9872a897b17057e0ac3976d8da007b
Brian Romaniuk <brirom@telus.net>
2021-09-01 17:32:23 -0700

    Test deploy

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 dotenv.config();
 const MongoClient = mongodb.MongoClient;
-const port = 5000 || 8000;
+const port = process.env.PORT || 8000;
 MongoClient.connect(
   'mongodb+srv://admin:7RYZIGcAEgJvnClB@cluster0.3uxtl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   {
@@ -25,7 +25,7 @@ MongoClient.connect(
     if (process.env.NODE_ENV === 'production') {
       app.use(express.static(path.join(__dirname, 'build')))
       app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
       })
     }
     await WeatherDAO.injectDB(client);
