4f9a8d401d22a5b7c9a7eb512d7b56d10993e30d
Brian Romaniuk <brirom@telus.net>
2021-09-02 13:22:31 -0700

    Fix routing to selected city when clicking from dropdown

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Autocomplete, { createFilterOptions } from '@material-ui/lab/Autocomplete';
 import { TextField } from '@material-ui/core';
-import { useHistory } from 'react-router-dom';
 import cityList from '../city-list';
 
 
@@ -11,14 +10,15 @@ const filterOptions = createFilterOptions({
   });
 
 function Search() {
-    const history = useHistory();
-    
+
     return (
         <Autocomplete
             id="combo-box"
             options={cityList}
             getOptionLabel={(option) => option.label}
-            getOptionSelected={(option, value) => { if(option.id === value.id) { history.push(`/weather/${option.country}/${option.city}`) }} }
+            onChange={(event, option) => {
+                window.location.href = `/weather/${option.country}/${option.city}`
+            }}
             style={{ width: 300 }}
             renderInput={(params) => <TextField {...params} label="Enter your city" variant="outlined" />}
             filterOptions={filterOptions}
